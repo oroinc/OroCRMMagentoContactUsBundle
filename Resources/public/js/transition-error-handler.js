8bb6eb13a69fe8c3367dd80eb6770277bb7b9ae6
AlexandrDmitriev <admitriev@magecore.com>
2014-07-11 14:56:02 +0300

    CRM-870: ContactUs Send Email action. Incorrect error processing

---
+++ Resources/public/js/transition-error-handler.js
@@ -0,0 +1,22 @@
+define([
+    'jquery',
+    'oroui/js/messenger',
+    'orotranslation/js/translator'
+], function ($, messenger, __) {
+        'use strict';
+        return {
+            handleEmailTransitionError: function() {
+                var element = $('#transition-orocrm_contact_us_contact_request-send_email');
+                element.off('transitions_failure');
+                element.on('transitions_failure', function(event, jqxhr){
+                    if (jqxhr.status != 500) {
+                        return;
+                    }
+                    event.stopImmediatePropagation();
+                    event.preventDefault();
+                    messenger.notificationFlashMessage('error', __('oro.email.handler.unable_to_send_email'));
+                });
+            }
+        }
+    }
+);
