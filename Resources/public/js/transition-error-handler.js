9037704cf9bbe2e4ea8015872df1255e786c6578
Hryhorii Hrebiniuk <hhrebiniuk@magecore.com>
2015-07-08 11:53:02 +0300

    BAP-8227: Autoformat JS - use external tool to format all JS code
     - jscs and jshint fixes

--- Resources/public/js/transition-error-handler.js
+++ Resources/public/js/transition-error-handler.js
@@ -16,7 +16,7 @@ define([
                 var element = $('#transition-orocrm_contact_us_contact_request-send_email');
                 element.off('transitions_failure');
                 element.on('transitions_failure', function(event, jqxhr) {
-                    if (jqxhr.status != 500) {
+                    if (jqxhr.status !== 500) {
                         return;
                     }
                     event.stopImmediatePropagation();
@@ -24,6 +24,6 @@ define([
                     messenger.notificationFlashMessage('error', __('oro.email.handler.unable_to_send_email'));
                 });
             }
-        }
+        };
     }
 );
